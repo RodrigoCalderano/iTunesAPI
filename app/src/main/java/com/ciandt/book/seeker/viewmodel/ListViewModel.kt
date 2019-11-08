1087050d955b1229c549e49f5770212cd44f9c3f
RodrigoCalderano <rodrigo.calderano@gmail.com>
2019-11-07 22:51:16 -0300

    layout mock added

--- app/src/main/java/com/ciandt/book/seeker/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/ciandt/book/seeker/viewmodel/ListViewModel.kt
@@ -1,6 +1,33 @@
 package com.ciandt.book.seeker.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ciandt.book.seeker.model.Book
 
 class ListViewModel: ViewModel() {
+
+    val books = MutableLiveData<List<Book>>()
+    val bookLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh(){
+        fetchBooks()
+    }
+
+    private fun fetchBooks(){
+        val mockData = listOf(
+            Book(name = "BookA", author = "Author A", description = "desc",
+                image160 = "Im160", image1100 = "Im1100"),
+            Book(name = "BookB", author = "Author B", description = "descb",
+                image160 = "Im160b", image1100 = "Im1100b"),
+            Book(name = "BookC", author = "Author C", description = "descc",
+                image160 = "Im160c", image1100 = "Im1100c"),
+            Book(name = "BookD", author = "Author D", description = "descd",
+                image160 = "Im160d", image1100 = "Im1100d")
+        )
+
+        bookLoadError.value = false
+        loading.value = false
+        books.value = mockData
+    }
 }
