145bcdd6cf2f13726689c0403ee6ef7673c86a69
RodrigoCalderano <rodrigo.calderano@gmail.com>
2019-11-09 03:22:17 -0300

    BookDetails receiving intent

--- app/src/main/java/com/ciandt/book/seeker/view/BookDetailsActivity.kt
+++ app/src/main/java/com/ciandt/book/seeker/view/BookDetailsActivity.kt
@@ -3,11 +3,16 @@ package com.ciandt.book.seeker.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ciandt.book.seeker.R
+import kotlinx.android.synthetic.main.activity_book_details.*
 
 class BookDetailsActivity : AppCompatActivity() {
+
+    val name : String by lazy { intent.getStringExtra("name")}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
+
+        textView.text = name
     }
 }
