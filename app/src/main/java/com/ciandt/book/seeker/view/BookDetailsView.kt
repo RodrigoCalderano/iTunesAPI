c0b858fa5a6f8e6a82b35f5088c95492277b69a5
RodrigoCalderano <rodrigo.calderano@gmail.com>
2019-11-09 17:42:12 -0300

    Details Refactored to fit MVVM

--- app/src/main/java/com/ciandt/book/seeker/view/BookDetailsView.kt
+++ app/src/main/java/com/ciandt/book/seeker/view/BookDetailsView.kt
@@ -1,56 +1,50 @@
 package com.ciandt.book.seeker.view
 
-import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.ciandt.book.seeker.R
-import com.ciandt.book.seeker.dao.BookServiceDAO
-import com.ciandt.book.seeker.model.Book
 import com.ciandt.book.seeker.util.getProgressDrawable
 import com.ciandt.book.seeker.util.loadImage
+import com.ciandt.book.seeker.viewmodel.BookDetailsViewModel
 import kotlinx.android.synthetic.main.activity_book_details.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.toast
 
 class BookDetailsView : AppCompatActivity() {
 
-    private val name: String by lazy { intent.getStringExtra("name") }
+    private val bookName: String by lazy { intent.getStringExtra("name") }
+
+    private lateinit var viewModel: BookDetailsViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
 
+        viewModel = ViewModelProviders.of(this).get(BookDetailsViewModel::class.java)
+        viewModel.fetchBookFromDb(bookName)
 
-        doAsync {
-            val book: Book? = BookServiceDAO.getBook(name)
-            uiThread {
-                if(book!=null){
-                    detailsTitle.text = book.name
-                    detailsAuthor.text = book.author
-                    val progressDrawable = getProgressDrawable(applicationContext)
-                    @Suppress("DEPRECATION")
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        detailsDesc.text = Html.fromHtml(book.description, Html.FROM_HTML_MODE_LEGACY).toString()
-                    } else detailsDesc.text = Html.fromHtml(book.description).toString()
+        observeViewModel()
+    }
 
-                    imageView.loadImage(book.image1100, progressDrawable)
-                }
+    private fun observeViewModel() {
+        viewModel.book.observe(this, Observer { book ->
+            book?.let {
+                detailsTitle.text = book.name
+                detailsAuthor.text = book.author
+                val progressDrawable = getProgressDrawable(applicationContext)
+                @Suppress("DEPRECATION")
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    detailsDesc.text =
+                        Html.fromHtml(book.description, Html.FROM_HTML_MODE_LEGACY).toString()
+                } else detailsDesc.text = Html.fromHtml(book.description).toString()
+                imageView.loadImage(book.image1100, progressDrawable)
             }
+        })
 
-
-//            val progressDrawable = getProgressDrawable(this)
-//            detailsTitle.text = name
-//            detailsAuthor.text = saved //TODO
-//            @Suppress("DEPRECATION")
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                detailsDesc.text = Html.fromHtml(description, Html.FROM_HTML_MODE_LEGACY).toString()
-//            } else detailsDesc.text = Html.fromHtml(description).toString()
-//
-//            imageView.loadImage(image1100, progressDrawable)
-
-        }
+        viewModel.errorMessage.observe(this, Observer { toast(it) })
     }
 }
---
+++ app/src/main/java/com/ciandt/book/seeker/viewmodel/BookDetailsViewModel.kt
@@ -0,0 +1,29 @@
+package com.ciandt.book.seeker.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ciandt.book.seeker.dao.BookServiceDAO
+import com.ciandt.book.seeker.model.Book
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class BookDetailsViewModel: ViewModel()  {
+
+
+    val book = MutableLiveData<Book>()
+    val errorMessage =  MutableLiveData<String>()
+
+    fun fetchBookFromDb(name: String) {
+
+        doAsync {
+            val bookRetrieved: Book? = BookServiceDAO.getBook(name)
+            uiThread {
+                    if (bookRetrieved != null){
+                        book.value = bookRetrieved
+                    } else {
+                        errorMessage.value = "Error has occurred while retrieving data"
+                    }
+            }
+        }
+    }
+}
