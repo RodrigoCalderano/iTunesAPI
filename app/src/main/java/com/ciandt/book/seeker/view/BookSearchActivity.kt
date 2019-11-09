24a012ebb297713438190dbe468c6b45e131d6a6
RodrigoCalderano <rodrigo.calderano@gmail.com>
2019-11-09 08:50:14 -0300

    Search refactor

--- app/src/main/java/com/ciandt/book/seeker/view/BookSearchActivity.kt
+++ app/src/main/java/com/ciandt/book/seeker/view/BookSearchActivity.kt
@@ -2,6 +2,8 @@ package com.ciandt.book.seeker.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.ciandt.book.seeker.R
 import org.jetbrains.anko.startActivity
 import kotlinx.android.synthetic.main.activity_book_search.*
@@ -11,6 +13,40 @@ class BookSearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_search)
-        button.setOnClickListener { startActivity<BookListView>("myParam" to 'a') }
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        button.setOnClickListener{
+            if (!isQueryValid(searchTxt.text!!)) {
+                textInputLayout.error = "Your query must be bigger"
+            } else {
+                textInputLayout.error = null
+                loginLoginBtnClicked()
+            }
+        }
+
+        val tw = object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (isQueryValid(searchTxt.text!!)) {
+                    textInputLayout.error = null
+                }
+            }
+        }
+
+        searchTxt.addTextChangedListener(tw)
+    }
+
+    private fun isQueryValid(text: Editable?): Boolean {
+        return text != null && text.length >= 2
+    }
+
+    private fun loginLoginBtnClicked() {
+        print("asdasddsaasd")
+        print(searchTxt.text)
+        startActivity<BookListView>("myParam" to 'a')
     }
 }
