8be023296313268deef55d9cce14f824dc08b91f
RodrigoCalderano <rodrigo.calderano@gmail.com>
2019-11-09 05:43:47 -0300

    Book details layout addded

--- app/src/main/java/com/ciandt/book/seeker/view/BookDetailsActivity.kt
+++ app/src/main/java/com/ciandt/book/seeker/view/BookDetailsActivity.kt
@@ -3,16 +3,26 @@ package com.ciandt.book.seeker.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ciandt.book.seeker.R
+import com.ciandt.book.seeker.util.getProgressDrawable
+import com.ciandt.book.seeker.util.loadImage
 import kotlinx.android.synthetic.main.activity_book_details.*
 
 class BookDetailsActivity : AppCompatActivity() {
 
-    val name : String by lazy { intent.getStringExtra("name")}
+    private val name : String by lazy { intent.getStringExtra("name")}
+    private val author : String by lazy { intent.getStringExtra("author")}
+    private val description : String by lazy { intent.getStringExtra("description")}
+    private val image1100 : String by lazy { intent.getStringExtra("image1100")}
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
 
-        textView.text = name
+        val progressDrawable = getProgressDrawable(this)
+        detailsTitle.text = name
+        detailsAuthor.text = author
+        detailsDesc.text = description
+        imageView.loadImage(image1100, progressDrawable)
     }
 }
