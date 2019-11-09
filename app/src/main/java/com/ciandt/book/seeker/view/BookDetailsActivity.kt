a470a92f6a6a8d315f8b4701fd250c46bec8ad3c
RodrigoCalderano <rodrigo.calderano@gmail.com>
2019-11-09 01:52:18 -0300

    BookDetails Added

---
+++ app/src/main/java/com/ciandt/book/seeker/view/BookDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.ciandt.book.seeker.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ciandt.book.seeker.R
+
+class BookDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_details)
+    }
+}
